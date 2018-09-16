1a0a521ff57de25019b8055ee56031514ca63c39
Autumn <autumncbullard@gmail.com>
2018-09-16 12:45:00 -0400

    Fix error

--- myApp.js
+++ myApp.js
@@ -55,7 +55,6 @@ app.use(express.static(`${__dirname}/public`))
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
 /** app.listen(process.env.PORT || 3000 ); */
-app.listen(process.env.PORT || 3000 );
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
