3106503d02fd2f94bc543f4314e1b628877e6513
Autumn <autumncbullard@gmail.com>
2018-09-16 12:23:29 -0400

    Revise syntax

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ console.log('Hello World');
 
 /** 3) Serve an HTML file */
 app.get('/', function(req, res) {
-  res.sendFile(absolutePath = `${__dirname}/views/index.html`);
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 /** 4) Serve static assets  */
