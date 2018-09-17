1cf2e332da73a4e9e830f18d9b5309f1ba8c323b
Autumn <autumncbullard@gmail.com>
2018-09-16 22:51:30 -0400

    Store a variable in the .env file that makes the JSON value alluppercase

--- myApp.js
+++ myApp.js
@@ -26,13 +26,18 @@ app.use(express.static(`${__dirname}/public`));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', function(req, res) {
-  res.json({
+  let data = {
     message: 'Hello json'
-  });
+  };
+
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    data.message = data.message.toUpperCase();
+  }
+  res.json(data);
 });
 
 /** 6) Use the .env file to configure the app */
-
+process.env.MESSAGE_STYLE='uppercase';
 
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
