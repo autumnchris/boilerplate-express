11a38f9793d7537d03eb7b807fafef82b46420dd
Autumn <autumncbullard@gmail.com>
2018-09-16 23:32:36 -0400

    Remove .env file

--- myApp.js
+++ myApp.js
@@ -25,6 +25,13 @@ app.get('/', function(req, res) {
 app.use(express.static(`${__dirname}/public`));
 
 /** 5) serve JSON on a specific route */
+/*app.get('/json', function(req, res) {
+  res.json({
+    message: 'Hello json'
+  });
+});*/
+
+/** 6) Use the .env file to configure the app */
 app.get('/json', function(req, res) {
   let data = {
     message: 'Hello json'
@@ -35,9 +42,6 @@ app.get('/json', function(req, res) {
   }
   res.json(data);
 });
-
-/** 6) Use the .env file to configure the app */
-process.env.MESSAGE_STYLE='uppercase';
 
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
